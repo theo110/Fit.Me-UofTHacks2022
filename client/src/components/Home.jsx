7379df8c4ee5f8b59edc60aeda745dd89ae55b4c
FreyasCode <freyazhang2@gmail.com>
2022-02-05 21:26:41 -0500

    added image to home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 // import HomePage from "./HomePage";
 // import './Home.css' 
 import Button from '../Button'
+import logo from '../background.png'
 
 function Home(props) {
 
     return (
         <>
             <h1 style = {{textAlign:"center"}}>Welcome</h1>
+            
+            <img src = {logo} alt = "Logo" height="650px" width="100%"/>;
             <Button/>
         </> 
+        
         )
 }
 
